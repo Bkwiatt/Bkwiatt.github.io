bb1379369ff56b22b812571799473acbfe1e9bc5
Brandon Kwiatkowski <bkwiat7@gmail.com>
2021-03-05 17:01:47 -0600

    fix mobile

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -42,6 +42,7 @@ export const VideoBg = styled.video`
     -o-object-fit: cover;
     object-fit: cover;
     background: #232a43;
+    -webkit-playsinline: autoPlay;
 `
 
 export const HeroContent = styled.div`
