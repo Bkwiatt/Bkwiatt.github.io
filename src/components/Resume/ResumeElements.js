d475e1e02acf4c6c649285aa976a74d243a6e01a
Brandon Kwiatkowski <bkwiat7@gmail.com>
2021-03-16 15:42:30 -0500

    recommit

--- src/components/Resume/ResumeElements.js
+++ src/components/Resume/ResumeElements.js
@@ -22,14 +22,45 @@ export const ResumeWrapper = styled.div`
 `
 
 export const ResumeCard = styled.div`
-    background: #fff;
+    background: #d3d3d3;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
-    align-items: center;
+    align-items: left;
+    height: 100%;
+    width: 100%;
     border-radius: 10px;
     padding: 30px;
     box-shadow: 0 1px 3px rgba(0,0,0,2);
     transition: all 0.2s ease-in-out;
+`
 
+export const ResumeH2 = styled.h2`
+`
+
+export const ResumeH4 = styled.h4`
+`
+
+export const ResumeHr = styled.hr`
+    background-color: '#000';
+    width: 50%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+`
+
+export const ResumeP = styled.p`
+`
+
+export const ResumeH5 = styled.h5`
+    padding-top: 10px;
+    padding-bottom: 10px;
+`
+
+export const ResumeA = styled.a`
+`
+export const ResumeImg = styled.img`
+    width: 300px;
+    height: 300px;  
+    padding-bottom: 20px;
+    align-self: center;
 `
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Video from '../../videos/video.mp4'
-import { ResumeContainer, ResumeWrapper, ResumeCard } from './ResumeElements'
+import { ResumeContainer, ResumeWrapper, ResumeCard, ResumeH2, ResumeH4, ResumeHr, ResumeP, ResumeH5, ResumeA, ResumeImg } from './ResumeElements'
 
 const ResumePage = () => {
     return (
@@ -8,25 +8,26 @@ const ResumePage = () => {
             <ResumeContainer>
                 <ResumeWrapper>
                     <ResumeCard>
-                        <h2>Brandon Kwiatkowski</h2>
-                        <h4>Programmer</h4>
-                        <hr/>
-                        <p>Highly motivated software developer constantly chasing improvement with a strong understanding of many core programming
+                        <ResumeImg src="https://cdn3.iconfinder.com/data/icons/business-round-flat-vol-1-1/36/user_account_profile_avatar_person_male-512.png" alt="avatar" />
+                        <ResumeH2>Brandon Kwiatkowski</ResumeH2>
+                        <ResumeH4>Programmer</ResumeH4>
+                        <ResumeHr/>
+                        <ResumeP>Highly motivated software developer constantly chasing improvement with a strong understanding of many core programming
                         concepts as well as a wide-range of languages, concepts and technologies. Supportive, likeable, and a responsible team player
-                            who’s dedicated to mastering his craft and never settling for anything less than top level performance.</p>
-                        <hr/>
-                        <h5>Location</h5>
-                        <p>Cary, IL</p>
-                        <h5>Phone</h5>
-                        <a href="tel:847-951-6388">(847) 951-6388</a>
-                        <h5>Email</h5>
-                        <a style={{ color: "black" }} href="mailto: Bkwiat7@gmail.com">Bkwiat7@gmail.com</a>
-                        <h5>Web</h5>
-                        <a href= "http://Bkwiatt.github.io">Bkwiatt.github.io</a>                        
-                        <hr/>
+                            who’s dedicated to mastering his craft and never settling for anything less than top level performance.</ResumeP>
+                        <ResumeHr/>
+                        <ResumeH5>Location</ResumeH5>
+                        <ResumeP>Cary, IL</ResumeP>
+                        <ResumeH5>Phone</ResumeH5>
+                        <ResumeA href="tel:847-951-6388">(847) 951-6388</ResumeA>
+                        <ResumeH5>Email</ResumeH5>
+                        <ResumeA style={{ color: "black" }} href="mailto: Bkwiat7@gmail.com">Bkwiat7@gmail.com</ResumeA>
+                        <ResumeH5>Web</ResumeH5>
+                        <ResumeA href= "http://Bkwiatt.github.io">Bkwiatt.github.io</ResumeA>                        
+                        <hResumeHr/>
                     </ResumeCard>
                         <ResumeCard>
-                        <h2>Education/Experience</h2>
+                        <ResumeH2>Education/Experience</ResumeH2>
 
                         </ResumeCard>
 
